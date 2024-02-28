a8ba429299b956bcc05f523e863e3efda990836f
Hector <redmagix2000@gmail.com>
2024-02-28 12:52:02 -0600

    struct and enum assignment 02 complete

--- assignments/02structenumpattern/target/debug/deps/assignment2-d3081ad06fed5ae5.d
+++ assignments/02structenumpattern/target/debug/deps/assignment2-d3081ad06fed5ae5.d
@@ -1,6 +1,6 @@
-C:\Users\hecto\Desktop\coding\rustprogramming\assignments\02structenumpattern\target\debug\deps\assignment2-d3081ad06fed5ae5.exe: main.rs structenum.rs
+C:\Users\hecto\Desktop\coding\HectorRust\rustprogramming\assignments\02structenumpattern\target\debug\deps\assignment2-d3081ad06fed5ae5.exe: main.rs structenum.rs
 
-C:\Users\hecto\Desktop\coding\rustprogramming\assignments\02structenumpattern\target\debug\deps\assignment2-d3081ad06fed5ae5.d: main.rs structenum.rs
+C:\Users\hecto\Desktop\coding\HectorRust\rustprogramming\assignments\02structenumpattern\target\debug\deps\assignment2-d3081ad06fed5ae5.d: main.rs structenum.rs
 
 main.rs:
 structenum.rs:
